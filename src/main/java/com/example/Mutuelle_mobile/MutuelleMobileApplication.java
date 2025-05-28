c72ded757850207b3f9e9b940099ce58dfdaf81d
Harold_Mk <mokamharoldroger@gmail.com>
2025-05-28 06:51:57 +0100

    Telechargement des dependances. Mais bon tout ca sera ignore lors de l'exportation vers github

---
+++ src/main/java/com/example/Mutuelle_mobile/MutuelleMobileApplication.java
@@ -0,0 +1,13 @@
+package com.example.Mutuelle_mobile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MutuelleMobileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MutuelleMobileApplication.class, args);
+	}
+
+}
